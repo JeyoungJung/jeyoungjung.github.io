824b3a024ac08e17fcbaa8d2aa0b44dcc4f97bf9
JeyoungJung <j74jung@uwaterloo.ca>
2021-01-02 02:11:10 -0500

    add resume

--- src/Skills.js
+++ src/Skills.js
@@ -2,6 +2,15 @@ import React from "react";
 import './App.css';
 
 function Skills () {
+
+  const style = {
+    color: 'yellow',
+    textTransform: 'none',
+    margin: '0px'
+  }
+
+  var resume = <a style={style} href="/resume.pdf" target="_blank"> link </a>
+
   return(
     <div className="skills-section" id="Skills">
       <div className="text-part">
@@ -21,6 +30,8 @@ function Skills () {
            <li>CSS</li>
          </ul>
         </p>
+        <hr/>
+        <p>Here is a {resume} to my resume if you are interested.</p>
       </div>
     </div>
   )
